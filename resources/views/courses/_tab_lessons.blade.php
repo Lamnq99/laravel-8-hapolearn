f29f377880754e100ba1e6ce1f543fa945150713
Lamnq19 <lam.nq@haposoft.com>
2021-09-03 22:29:19 +0700

    today

--- resources/views/courses/_tab_lessons.blade.php
+++ resources/views/courses/_tab_lessons.blade.php
@@ -10,28 +10,43 @@
     </form>
     <div class="col-lg-4 pr-0 align-self-center btn-join-container">
         @if (Auth::check() && $isJoined == true)
-        <a href="#" class="btn-join-course" id="btn-joined-course">Joined the course</a>
+            <a href="#" class="btn-join-course" id="btn-joined-course">Joined the course</a>
         @else
-        <a href="/insert/{{$course->id}}" class="btn-join-course" id="btn-join-course">Join the course</a>
+            <a href="/insert/{{ $course->id }}" class="btn-join-course" id="btn-join-course">Join the course</a>
         @endif
     </div>
 </div>
 <div class="row m-0 show-lessons-container">
     <div class="col-lg-12">
         @foreach ($lessons as $key => $lesson)
-        <div class="row">
-            <div class="col-lg-8 pr-0">
-                <p class="txt-title-lessons">{{$key + 1}}. {{$lesson->title}}</p>
-            </div>
-            <div class="col-lg-4 pl-0 btn-more-lessons">
-                @if (Auth::check() && $isJoined == true)
-                <a href="/allcourses/coursedetail/lesson/{{$lesson->id}}">Learn</a>
+            <div class="row">
+                <div class="col-lg-8 pr-0">
+                    <p class="txt-title-lessons">{{ $key + 1 }}. {{ $lesson->title }}</p>
+                </div>
+                <div class="col-lg-4 pl-0 btn-more-lessons">
+                    @if (Auth::check() && $isJoined == true)
+                        
+                            @if ($totalDocuments[0]->lesson_id == $lesson->id)
+                                @if ($learnedPart == 0)
+                                <a href="/allcourses/coursedetail/lesson/{{ $lesson->id }}">Learn</a>
+                                @elseif ($learnedPart > 0 && $learnedPart < 1) 
+                                <i class="fas fa-check-circle icon-check-learning"></i>
+                                <a href="/allcourses/coursedetail/lesson/{{ $lesson->id }}">Learning
+                                @elseif ($learnedPart == 1)
+                                <i class="fas fa-check-circle icon-check-learned"></i>
+                                <a href="/allcourses/coursedetail/lesson/{{ $lesson->id }}">Learned</a>
+                                @endif
+                            @else
+                                <a href="/allcourses/coursedetail/lesson/{{ $lesson->id }}">Learn</a>
+                            @endif
+
+                    
                 @endif
             </div>
         </div>
-        @endforeach
-    </div>
+    @endforeach
+</div>
 </div>
 <div class="pagination-container">
-    {{ $lessons->appends($_GET)->links('pagination::bootstrap-4') }}
+{{ $lessons->appends($_GET)->links('pagination::bootstrap-4') }}
 </div>
